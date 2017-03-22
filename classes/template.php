1f503342c2a325d8d942ad42ddc03ff265258276
karl-oskar.reiljan <karl-oskar.reiljan@khk.ee>
2017-03-22 12:21:03 +0200

    Koostatud template klassi esmaversioon

---
+++ classes/template.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: karl-oskar.reiljan
+ * Date: 22.03.2017
+ * Time: 12:14
+ */
+
+class template
+{// klassi algus
+    // template klassi omadused - muutujad
+    var $file =''; // html faili nimi
+    var $content = false; // html malli faili sisu
+    var $vars = array(); // html vaade sisu - reaalsed väärtused
+}klassi lõpp
+?>
