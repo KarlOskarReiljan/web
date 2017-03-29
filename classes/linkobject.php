6f8531faad6d18f61f89db7e7b74be34dad42897
karl-oskar.reiljan <karl-oskar.reiljan@khk.ee>
2017-03-29 14:35:13 +0300

    Lisatud põhilingi loomise võimalused

--- classes/linkobject.php
+++ classes/linkobject.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Created by PhpStorm.
  * User: karl-oskar.reiljan
@@ -7,6 +8,17 @@
  */
 class linkobject extends http
 {// klassi algus
+    // klassi muutujad - omadused
+    var $baseUrl = false;
+    var $delim = '%amp;';
+    var $eq = '=';
+    var $protocol = 'http://';
 
-
+    // klassi meetodid
+    // klassi konstruktor
+    function __construct(){
+        parent::__construct(); // kutsume tööle http konstruktor
+        // loome põhilink
+        $this->baseUrl = $this->protocol.HTTP_HOST.SCRIPT_NAME;
+    }// konstruktor
 }// klassi lõpp
