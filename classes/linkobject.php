9a6690b33477e30554af78b7f5ed7a0dfc2b3a25
karl-oskar.reiljan <karl-oskar.reiljan@khk.ee>
2017-03-29 15:10:42 +0300

    Lisatud täislingi koostamine

--- classes/linkobject.php
+++ classes/linkobject.php
@@ -10,7 +10,7 @@ class linkobject extends http
 {// klassi algus
     // klassi muutujad - omadused
     var $baseUrl = false;
-    var $delim = '%amp;';
+    var $delim = '&amp;';
     var $eq = '=';
     var $protocol = 'http://';
 
@@ -24,11 +24,25 @@ class linkobject extends http
 
     // andmete paari koostamine kujul
     // nimi=väärtus&nimi1=väärtus1 jne
-    function addToLink($link, $name, $val){
-        if($link !=''){
-            $link = $link.$this->$delim;
+    function addToLink(&$link, $name, $val){
+        if($link != ''){
+            $link = $link.$this->delim;
         }
         $link = $link.fixUrl($name).$this->eq.fixUrl($val);
-        echo $link;
     }// addToLink
+
+    // saame täislink valmis
+    function getLink($add = array()){
+        $link = '';
+        foreach($add as $name=>$val){
+            $this->addToLink($link, $name, $val);
+        }
+        if($link != ''){
+            $link = $this->baseUrl.'?'.$link;
+        } else {
+            $link = $this->baseUrl;
+        }
+        return $link;
+    }// getLink
+
 }// klassi lõpp
