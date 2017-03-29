e4fe239f3e80de294b0a1eced98c97abafb7696f
karl-oskar.reiljan <karl-oskar.reiljan@khk.ee>
2017-03-29 12:23:55 +0300

    template klassi fail on kasutusele võetud

--- conf.php
+++ conf.php
@@ -6,8 +6,8 @@
  * Time: 15:24
  */
 // defineerime vajalikud konstandid
-define ('CLASSES_DIR', 'classes/'); // classes kataloogi nime konstant
-define ('TMPL_DIR', 'tmpl/'); // tmpl kataloogi nime konstant
+define('CLASSES_DIR', 'classes/'); // classes kataloogi nime konstant
+define('TMPL_DIR', 'tmpl/'); // tmpl kataloogi nime konstant
 
 // võtame kasutusele vajalikud failid
 require_once CLASSES_DIR.'template.php';
