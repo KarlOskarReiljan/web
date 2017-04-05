18c73d484c0f6d61fd1ee83ceedc31f58daf1294
karl-oskar.reiljan <karl-oskar.reiljan@khk.ee>
2017-04-05 09:32:36 +0300

    Täiendatud vaikimisi tegevuse kirjelduse

--- act.php
+++ act.php
@@ -13,6 +13,7 @@ if(file_exists($fn) and is_file($fn) and is_readable($fn)){
     // loeme sisu
     require_once $fn;
 } else {
-    $fn = ACTS_DIR.'default'.'.php';
+    $fn = ACTS_DIR.'default'.'.php'; // koostame vaikimisi oleva faili nimi
+    $http->('act', DEFAULT_ACT); // paneme act väärtuseks default - act=default
     require_once $fn;
 }
