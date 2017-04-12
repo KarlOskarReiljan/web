c6d70003aee6dc71939143e250b68871bbf8fa88
karl-oskar.reiljan <karl-oskar.reiljan@khk.ee>
2017-04-12 12:03:34 +0300

    Koostatud keeleriba väljastamine

---
+++ lang.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: karl-oskar.reiljan
+ * Date: 12.04.2017
+ * Time: 12:02
+ */
+// loome keele elementide eraldaja
+$sep = new template('lang.sep');
+$sep = $sep->parse();
+$count = 0;
+// väljastame kõik olemasolevad keeled
+foreach ($siteLangs as $lang_id=>$lang_name){
+    $count++;
+    // paneme keele id järgi aktiivne element
+    if($lang_id == LANG_ID){
+        $item = new template('lang.active');
+    } else {
+        $item = new template('lang.item');
+    }
+    // koostame keele riba väljund
+    $link = $http->getLink(array('lang_id'=>$lang_id), array('act', 'page_id'), array('lang_id'));
+    $item->set('link', $link);
+    $item->set('name', $lang_name);
+    $main_tmpl->add('lang_bar', $item->parse());
+
+    if($count < count($siteLangs)){
+        $main_tmpl->add('lang_bar', $sep);
+    }
+}
