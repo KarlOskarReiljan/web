be91a6dbc49ac1ca64fb2649db2b1dee086027b6
karl-oskar.reiljan <karl-oskar.reiljan@khk.ee>
2017-04-12 08:59:08 +0300

    Koostatud andmetega päringu töötlus

--- classes/mysql.php
+++ classes/mysql.php
@@ -43,5 +43,21 @@ class mysql
         }
         return $res;
     }// query
+
+    // andmetega päringu teostamine
+    function getArray($sql){
+        $res = $this->query($sql);
+        $data = array();
+        while($row = mysqli_fetch_assoc($res)){
+            $data[] = $row;
+        }
+        if(count($data) == 0){
+            return false;
+        }
+        return $data;
+    }// getArray
+
+
+
 } // klassi lõpp
 ?>
