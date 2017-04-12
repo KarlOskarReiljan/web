9fef56c07bfa1e5fd8a890667009fa26fe3a68e1
karl-oskar.reiljan <karl-oskar.reiljan@khk.ee>
2017-04-12 12:15:55 +0300

    Koostatud tõlkifunktsiooni esmaversioon

---
+++ lib/trans.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: karl-oskar.reiljan
+ * Date: 12.04.2017
+ * Time: 12:11
+ */
+function tr($txt){
+    static $trans = false;
+
+    if(LANG_ID == DEFAULT_LANG){
+        return $txt;
+    }
+
+    if($trans === false){
+        $fn = LANG_DIR.'lang_'.$lang_id.'.php';
+        if(file_exists($fn) and is_file($fn) and is_readable($fn)){
+            require_once $fn;
+            $trans = $_trans;
+        } else {
+            $trans = array();
+        }
+        if(isset($trans[$txt])){
+            return $trans[$txt];
+        }
+    }
+    return $txt;
+}
