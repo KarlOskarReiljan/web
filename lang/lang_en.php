0500da9778d4319aba20fd6acfa87b7127a11837
karl-oskar.reiljan <karl-oskar.reiljan@khk.ee>
2017-04-12 12:10:45 +0300

    Koostatud inglise keelse tõlge esmaversioon

---
+++ lang/lang_en.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: karl-oskar.reiljan
+ * Date: 12.04.2017
+ * Time: 12:09
+ */
+$_trans = array(
+    'Esimene leht' => 'First page',
+    'Teine leht' => 'Second page',
+    'avaleht' => 'main page',
+    'Lehe peamenüü' => 'Page menu'
+);
