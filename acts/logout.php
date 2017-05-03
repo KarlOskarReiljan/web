a4bcdd2d70e1a98d6989f4c704737dc89403945d
karl-oskar.reiljan <karl-oskar.reiljan@khk.ee>
2017-05-03 11:09:20 +0300

    Lisatud kasutaja väljalogimine

---
+++ acts/logout.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: karl-oskar.reiljan
+ * Date: 3.05.2017
+ * Time: 11:07
+ */
+$sess->delSession(); // kustutame olemas olev sessioon
+$http->redirect();// suuname pealehele
+?>
