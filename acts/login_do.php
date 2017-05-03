883074e9d0fd6e44b915ccfb1e9a3cda122b5c58
karl-oskar.reiljan <karl-oskar.reiljan@khk.ee>
2017-05-03 10:43:53 +0300

    Lisatud kasutaja suunamine sisse logimisvormile

---
+++ acts/login_do.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: karl-oskar.reiljan
+ * Date: 3.05.2017
+ * Time: 9:38
+ */
+// võtame kätte vormi poolt edastatud andmed
+$username = $http->get('kasutaja');
+$passwd = $http->get('parool');
+
+// koostame päringu kasutaja kontrollimiseks andmebaasis
+$sql = 'SELECT * FROM user'.
+    'WHERE username='.fixDb($username).
+    ' AND password ='.fixDb(md5($passwd));
+$res = $db->getArray($sql);
+
+// teeme päringu tulemuse kontroll
+if($res == false){
+    // siis tuleb suunata kasutaja sisselogimisvormi tagasi
+    £link = $http->getLink(array('act' => 'login'));
+    $http->redirect($link);
+} else {
+    // sisse tuleb avada kasutajale sessiooni
+    $sess->createSession($res[0]);
+    // tuleb suunata kasutajat pealehele
+    // kus ma väljastan kasutajaandmed sessiooni kontrolliks
+    $http->redirect();
+}
